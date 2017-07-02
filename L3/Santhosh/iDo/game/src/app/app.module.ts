28de2d8d09bd713841f9a2d081e832100f6e9c40
Santhosh Gugan <santhoshg1990@gmail.com>
2017-07-02 15:43:59 +0530

    Angular CLI

--- L3/Santhosh/iDo/game/src/app/app.module.ts
+++ L3/Santhosh/iDo/game/src/app/app.module.ts
@@ -4,31 +4,19 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {Component, Input, Output, EventEmitter} from '@angular/core';
-import {JokeComponent,JokeListComponent,JokeFormComponent } from './app.component';
+import {AppComponent,JokeComponent,JokeListComponent,JokeFormComponent } from './app.component';
 
 @NgModule({
   imports: [BrowserModule,FormsModule,HttpModule],
   declarations: [
     JokeComponent,
     JokeListComponent,
-    JokeFormComponent
+    JokeFormComponent,
+    AppComponent
   ],
   bootstrap: [AppComponent]
 })
-export class AppComponent {
+export class AppModule {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
